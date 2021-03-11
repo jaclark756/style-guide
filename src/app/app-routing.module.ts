c9413072251d1d98e37255bb0ff039f821de6a56
Cale Cortney <thortney@mailbox.org>
2021-03-11 06:12:15 -0600

    removed unused component, added wireframes tab to sidenav, changed page-height

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,13 @@ import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './components/home/home.component';
 import { InputComponent } from './components/input/input.component';
 import { ColorComponent } from './components/color/color.component';
-import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  // {path: 'components/header', component: HeaderComponent},
   {path: 'components/button', component: ButtonComponent},
   {path: 'components/card', component: CardComponent},
   {path: 'components/input', component: InputComponent},
-  {path: 'components/color', component: ColorComponent},
+  {path: 'wireframes', component: HomeComponent},
 
 ];
 
