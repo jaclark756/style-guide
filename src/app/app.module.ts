6d5dfb81f47d420bb1fd5b197cdf106d096c398c
Cale Cortney <thortney@mailbox.org>
2021-03-11 19:28:50 -0600

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,11 +24,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
