8d0ba2cadb2486211588b007f08157b88d79b1ba
Cale Cortney <thortney@mailbox.org>
2021-03-12 20:53:43 -0600

    fixed header import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './components/home/home.component';
 import { InputComponent } from './components/input/input.component';
 import { ColorComponent } from './components/color/color.component';
-import { HeaderComponent } from './components/header/header.component';
 import { TypographyComponent } from './components/typography/typography.component';
 import { WireframesComponent } from './components/wireframes/wireframes.component';
 
